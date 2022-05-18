b52cc5154b563adef15681c531974b1eb56354a6
charlie <chlam98@gmail.com>
2022-05-18 15:42:55 -0400

    added functions

--- word_jumble.py
+++ word_jumble.py
@@ -5,39 +5,43 @@
 
 import random
 
-# create a sequence of words to choose from
-WORDS = ("python", "jumble", "easy", "difficult", "answer", "xylophone")
-# pick one word randomly from the sequence
-word = random.choice(WORDS)
-# create a variable to use later to see if the guess is correct
-correct = word
+def startGuessing():
+    # create a sequence of words to choose from
+    WORDS = ("python", "jumble", "easy", "difficult", "answer", "xylophone")
+    # pick one word randomly from the sequence
+    word = random.choice(WORDS)
+    # create a variable to use later to see if the guess is correct
+    correct = word
 
-# create a jumbled version of the word
-jumble = ""
-while word:
-    position = random.randrange(len(word))
-    jumble += word[position]
-    word = word[:position] + word[(position + 1):]
+    # create a jumbled version of the word
+    jumble = ""
+    while word:
+        position = random.randrange(len(word))
+        jumble += word[position]
+        word = word[:position] + word[(position + 1):]
 
-# start the game
-print(
-    """
-               Welcome to Word Jumble!
-    
-       Unscramble the letters to make a word.
-    (Press the enter key at the prompt to quit.)
-    """
-)
-print("The jumble is:", jumble)
+    # start the game
+    print(
+        """
+                   Welcome to Word Jumble!
 
-guess = input("\nYour guess: ")
-while guess != correct and guess != "":
-    print("Sorry, that's not it.")
-    guess = input("Your guess: ")
+           Unscramble the letters to make a word.
+        (Press the enter key at the prompt to quit.)
+        """
+    )
+    print("The jumble is:", jumble)
 
-if guess == correct:
-    print("That's it!  You guessed it!\n")
+    guess = input("\nYour guess: ")
+    while guess != correct and guess != "":
+        print("Sorry, that's not it.")
+        guess = input("Your guess: ")
 
-print("Thanks for playing.")
+    if guess == correct:
+        print("That's it!  You guessed it!\n")
 
-input("\n\nPress the enter key to exit.")
+    print("Thanks for playing.")
+
+    input("\n\nPress the enter key to exit.")
+
+if __name__ == "__main__":
+    startGuessing()
